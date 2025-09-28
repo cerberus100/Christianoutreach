a1da3438a1e9c3032252e1286caf386c3b9f86ef
AnarchoFatSats <alexsiegel@Alexs-MacBook-Pro.local>
2025-09-28 00:02:24 -0700

    Add missing AUTH_COOKIE_NAMES import to auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import jwt from 'jsonwebtoken';
 import { parse } from 'cookie';
+import { AUTH_COOKIE_NAMES } from '@/types';
 
 /**
  * JWT payload interface
